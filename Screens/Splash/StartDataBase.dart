92aa6199a4b1d8fade625946c9638af4b122bd36
Mostafa Mahmoud <mshoman01020@gmail.com>
2023-08-29 18:19:41 +0300

    StartingDataBase

---
+++ Screens/Splash/StartDataBase.dart
@@ -0,0 +1,44 @@
+
+import 'package:orangera/DataBase/DataBaseLogic.dart';
+
+insertFood(DataBaseLogic obj){
+  obj.insert(category: 'food', name: 'Italiano Pasta Ziti - 400 Gram', calories: '360 kcal/100gm', price: 'EGP 14.95', count: 1);
+  obj.insert(category: 'food', name: 'Lamar Full Cream Milk - 1 Liter', calories: '120 kcal/100gm', price: 'EGP 34.75', count: 1);
+  obj.insert(category: 'food', name: 'Juhayna Full Cream Milk - 1.5 Liter', calories: '114 kcal/100gm', price: 'EGP 49.50', count: 1);
+  obj.insert(category: 'food', name: 'Al Doha Egyptian Rice - 1kg', calories: '350 kcal/100gm', price: 'EGP 30.95', count: 1);
+  obj.insert(category: 'food', name: 'Domty Feta Cheese, Low Salt - 1 kg', calories: '240 kcal/100gm', price: 'EGP 83.10', count: 1);
+  obj.insert(category: 'food', name: 'Dolphin Tuna Shredded - 200 Gram', calories: '200 kcal/100gm', price: 'EGP 24.98', count: 1);
+  obj.insert(category: 'food', name: 'Almarai Beef Burger - 1kg', calories: '140 kcal/100gm', price: 'EGP 89.95', count: 1);
+  obj.insert(category: 'food', name: 'Koki Beef Hot Dog - 30 Pieces', calories: '166 kcal/100gm', price: 'EGP 113.95', count: 1);
+  obj.insert(category: 'food', name: 'Americana Beef Burger - 30 Pieces', calories: '140 kcal/100gm', price: 'EGP 86.95', count: 1);
+  obj.insert(category: 'food', name: 'Balady Beef Neck - 1 kg', calories: '299 kcal/100gm', price: 'EGP 333.95', count: 1);
+  obj.insert(category: 'food', name: 'Chicken Breast Fillet - 1kg', calories: '195 kcal/100gm', price: 'EGP 199.95', count: 1);
+  obj.insert(category: 'food', name: 'Large Baladi Bread - 1 piece', calories: '150 kcal/100gm', price: 'EGP 3.35', count: 1);
+  obj.insert(category: 'food', name: 'Red Bell Pepper - 1 kg', calories: '30 kcal/100gm', price: 'EGP 39.95', count: 1);
+  obj.insert(category: 'food', name: 'Cucumber - 1 kg', calories: '15 kcal/100gm', price: 'EGP 19.95', count: 1);
+  obj.insert(category: 'food', name: 'Balady Tomatoes - 1 kg', calories: '20 kcal/100gm', price: 'EGP 14.95', count: 1);
+  obj.insert(category: 'food', name: 'Date Barhy - 1 kg', calories: '283 kcal/100gm', price: 'EGP 35.95', count: 1);
+  obj.insert(category: 'food', name: 'Potatoes For Frying - 1 kg', calories: '274 kcal/100gm', price: 'EGP 16.95', count: 1);
+  obj.insert(category: 'food', name: 'Shelly Mango - 1 kg', calories: '60 kcal/100gm', price: 'EGP 27.95', count: 1);
+  obj.insert(category: 'food', name: 'Premium White Eggs - 30 Egg', calories: '51 kcal/100gm', price: 'EGP 130.95', count: 1);
+  obj.insert(category: 'food', name: 'Premium Red Eggs - 30 Egg', calories: '147 kcal/100gm', price: 'EGP 132.95', count: 1);
+  obj.insert(category: 'food', name: 'Romy Batarekh Cheese - 1 kg', calories: '350 kcal/100gm', price: 'EGP 269.95', count: 1);
+  obj.insert(category: 'food', name: 'Red Apples - 1 kg', calories: '52 kcal/100gm', price: 'EGP 99.95', count: 1);
+  obj.insert(category: 'food', name: 'Imported Banana - 1 kg', calories: '89 kcal/100gm', price: 'EGP 69.95', count: 1);
+  obj.insert(category: 'food', name: 'Sweet Potatoes - 1 kg', calories: '90 kcal/100gm', price: 'EGP 9.95', count: 1);
+  obj.insert(category: 'food', name: 'Carrot - 1 kg', calories: '54 kcal/100gm', price: 'EGP 24.95', count: 1);
+  obj.insert(category: 'food', name: 'Birell Malt Drink - 330ml', calories: '16 kcal/100gm', price: 'EGP 9.50', count: 1);
+  obj.insert(category: 'food', name: 'Pepsi Cola Can - 330ml', calories: '41 kcal/100gm', price: 'EGP 9.75', count: 1);
+  obj.insert(category: 'food', name: 'Fayrouz Pineapple Malt Drink - 330ml', calories: '114 kcal/100gm', price: 'EGP 9.25', count: 1);
+  obj.insert(category: 'food', name: 'Coca Cola Can - 330ml', calories: '42 kcal/100gm', price: 'EGP 9.75', count: 1);
+  obj.insert(category: 'food', name: 'Koki Plain Beef Luncheon - 1 kg', calories: '353 kcal/100gm', price: 'EGP 149.95', count: 1);
+  obj.insert(category: 'food', name: 'Fresh Istamboly Cheese - 1 kg', calories: '61 kcal/100gm', price: 'EGP 94.95', count: 1);
+  obj.insert(category: 'food', name: 'Barshoumy Fig - 1 kg', calories: '249 kcal/100gm', price: 'EGP 34.95', count: 1);
+  obj.insert(category: 'food', name: 'Shoky Fig', calories: '74 kcal/100gm', price: 'EGP 4.95', count: 1);
+  obj.insert(category: 'food', name: 'Black Grapes - 1 kg', calories: '73 kcal/100gm', price: 'EGP 34.95', count: 1);
+  obj.insert(category: 'food', name: 'Balady Lemons - 1 kg', calories: '29 kcal/100gm', price: 'EGP 14.95', count: 1);
+  obj.insert(category: 'food', name: 'Tilapia Fish - 1 kg', calories: '128 kcal/100gm', price: 'EGP 99.95', count: 1);
+  obj.insert(category: 'food', name: 'Grouper Fish Fillet', calories: '232 kcal/100gm', price: 'EGP 314.95', count: 1);
+  obj.insert(category: 'food', name: 'Small Crab Female - 1 kg', calories: '97 kcal/100gm', price: 'EGP 174.95', count: 1);
+  obj.insert(category: 'food', name: 'Pomegranate - 1 kg', calories: '234 kcal/100gm', price: 'EGP 28.95', count: 1);
+}
